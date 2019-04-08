6d119374b7151557414f68e6f81f970d5e167a0a
jadis1294 <lucads1294>
2019-04-08 17:18:45 +0200

    aggiunta cartella con futura libreria

---
+++ Clustered_Graph/clustered_graph.js
@@ -0,0 +1,29 @@
+/**
+ * This function for the object Underlying_graph.
+ * @param {label} the label of the graph
+ * @param {embedded} boolean value to decide if the rotationScheme of the node should be read
+ * @param {clusters} array of the cluster in the graph
+ * @param {nodes} array of the nodes in the graph
+ * @param {edges} array of the edges in the graph
+ */
+
+function underlying_graph(label,embedded,clusters,nodes,edges) = {
+    this.label= label,
+    this.embedded= embedded,   //se è embedded il rotationScheme va letto, altrimenti no
+    this.clusters=clusters,
+    this.nodes= nodes,
+    this.edges=edges,
+
+}          
+
+/**
+ * This function for the object inclusion_tree.
+ * @param {label} the label of the graph
+ * @param {embedded} boolean value to decide if the rotationScheme of the node should be read
+ * @param {clusters} array of the cluster in the graph
+ * @param {nodes} array of the nodes in the graph
+ * @param {edges} array of the edges in the graph
+ */
+function inclusion_tree()={
+    
+}       
